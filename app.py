fc0592ffe25bda4f337d5fc95627404c8c19b6d2
ntandazomhlabeni <ntandazomhlabeni@auctioneer.net>
2025-07-27 04:46:36 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from world web server. Your job script runs separately."
+
+if __name__ == '__main__':
+    # Render automatically sets the PORT environment variable.
+    # We bind to 0.0.0.0 to make it accessible from outside the container.
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
